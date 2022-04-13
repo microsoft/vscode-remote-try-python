33d8eda2f55a3f3a4433bb2db8023d03f41cd742
berlincooke <berlincooke@gmail.com>
2022-04-13 11:53:13 +0000

    Testando Integração VS Code com Github Fixes #45

--- app2.py
+++ app2.py
@@ -1,1 +1,8 @@
 print('pip install -r requirements.txt')
+
+#Aprender a fazer um arquivo readme para explicar o código README.md
+#Objetivo: criar pasta linkada entre Github, Docker e VsCode
+#Objevito2: instalar bibliotecas específicas do python neste env. e testar. Testar integração com Github.
+
+
+
